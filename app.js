48e91b888eeec30f46d1f24132962f2f0280de62
majdzaatri <majdz-1@live.com>
2020-05-16 23:05:42 +0300

    added intiale design for sign in, dashboard pages and a simple backend to run the website on a server

--- app.js
+++ app.js
@@ -6,10 +6,26 @@ app.use(bodyParser.urlencoded({urlencoded: true}));
 app.use('/public', express.static(__dirname+'/public'));
 
 app.get('/', function(req,res){
-    res.send("Hello World");
-})
+    res.redirect(301, '/sign-in');
+});
 
-const port = "8080";
+app.get('/sign-in', function(req,res){
+    res.sendFile(__dirname + '/views/sign-in.html');
+});
+
+app.get('/sign-up', function(req,res){
+    res.sendFile(__dirname + '/views/sign-up.html');
+});
+
+app.get('/reset-password', function(req,res){
+    res.sendFile(__dirname + '/views/reset-password.html');
+});
+
+app.get('/dashboard', function(req,res){
+    res.sendFile(__dirname + '/views/dashboard.html');
+});
+
+const port = "8000";
 const host = "localhost";
 
 const server = app.listen(port, host, () => {
