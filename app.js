77fdee6bdc438ec89c58ac21edb7cad396219c5c
Fadi Habeeb <fadi2habeeb@gmail.com>
2020-06-01 23:25:55 +0300

    netfly

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ var passport = require('passport')
 passport.use(new FacebookStrategy({
     clientID: '978507335946498',
     clientSecret: '6680fb4eb4e4a3879b51a94e966ac353',
-    callbackURL: "http://localhost:5000/auth/facebook/callback",
+    callbackURL: "http://csp-braude.herokuapp.com/auth/facebook/callback",
     profileFields : ['id', 'displayName', 'email']
 
 },
