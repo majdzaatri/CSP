ee31e75fbd8aff74b5b0042992c08b12d47643b8
majdzaatri <majdz-1@live.com>
2020-09-26 19:49:33 +0300

    fixing dotenv on heroku

--- account_manager.js
+++ account_manager.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 const crypto = require("crypto");
 const mysql = require("mysql");
 const https = require("https");
