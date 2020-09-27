7126f9743bf8a6b9ee5277e6bf5935c273493d6f
majdzaatri <majdz-1@live.com>
2020-09-27 13:09:28 +0300

    fixing dotenv

--- account_manager.js
+++ account_manager.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: '/.env' });
+require("dotenv").config({ path: '.env' });
 const crypto = require("crypto");
 const mysql = require("mysql");
 const https = require("https");
